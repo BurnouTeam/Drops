ccfc89129f2735d7886972c2a012677c5973c684
Dante de Araújo <danteeng@hotmail.com>
2024-12-22 19:58:30 -0300

    :heavy_plus_sign: chore(deps): add vite-plugin-svgr as dependency for using svgs as react components

---
+++ src/data/orders.ts
@@ -0,0 +1,48 @@
+export const orders = {
+  pending: [
+    {
+      id: '34567',
+      product: 'Serra Grande 20L',
+      customer: 'Rafael Costa Almeida',
+      phone: '+55 85 988012345',
+      address: 'Rua Baturité, 1221, Parangaba',
+      price: '13,00',
+      payment: 'Pix',
+      time: '09:59',
+    },
+    {
+      id: '34568',
+      product: 'Naturágua 20L',
+      customer: 'Carolina Mendes Oliveira',
+      phone: '+55 85 988012345',
+      address: 'Av. Godofredo Maciel, 1010, Maraponga',
+      price: '13,00',
+      payment: 'Pix',
+      time: '09:59',
+    },
+  ],
+  shipped: [
+    {
+      id: '34569',
+      product: 'Naturágua 20L',
+      customer: 'Carolina Mendes Oliveira',
+      phone: '+55 85 988012345',
+      address: 'Av. Godofredo Maciel, 1010, Maraponga',
+      price: '13,00',
+      payment: 'Pix',
+      time: '09:59',
+    },
+  ],
+  completed: [
+    {
+      id: '34570',
+      product: 'Pacoty 20L',
+      customer: 'Carolina Mendes Oliveira',
+      phone: '+55 85 988012345',
+      address: 'Av. Godofredo Maciel, 1010, Maraponga',
+      price: '26,00',
+      payment: 'Cartão de Crédito',
+      time: '09:59',
+    },
+  ],
+};
