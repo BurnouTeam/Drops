9f096731ff0c254c7b034d427ddbd1516a1336d6
Dante de Araújo <danteeng@hotmail.com>
2025-01-21 10:07:16 -0300

    :clown_face: chore(clients): mock clients information in a specific file

--- src/data/clients.ts
+++ src/data/clients.ts
@@ -1,13 +1,15 @@
 
 const clients: Client[] = [
   { name: "Dante Araújo", phone: "+55 85 996105145", address:"Rua Antonele Bezerra, 255, Meireles"},
-  { name: 'Drielle Furtado', phone: '+55 85 996101020', address: 'Rua Ana Neri, 1010, Parangaba' },
-  { name: 'Filipe Cavalcante', phone: '+55 85 996102553', address: 'Av Washington Soares, 1010, Edson Queiroz' },{ name: 'Rafael Costa Almeida', phone: '+55 85 988012345', address: 'Rua Baturité, 1221, Parangaba' },
+  { name: 'Drielle Furtado', phone: '+55 85 987552384', address: 'Rua Ana Neri, 1010, Parangaba' },
+  { name: 'Filipe Cavalcante', phone: '+55 85 991094233', address: 'Av Washington Soares, 1010, Edson Queiroz' },{ name: 'Rafael Costa Almeida', phone: '+55 85 988012345', address: 'Rua Baturité, 1221, Parangaba' },
   { name: 'Carolina Mendes Oliveira', phone: '+55 85 988012343', address: 'Av. Godofredo Maciel, 1010, Maraponga' },
   { name: 'Jordana Mendes', phone: '+55 85 981111215', address: 'Rui Barbosa, 1010, Aldeota' },
   { name: 'Roberto Carvalho', phone: '+55 85 996320041', address: 'Rua José Vilar, 2013, Aldeota' },
   { name: 'Livia Rodrigues', phone: '+55 85 999012555', address: 'Rua Heráclito Graça, 261, Centro' },
   { name: 'Ilana Ramos', phone: '+55 85 99624536', address: 'Rua Silva Paulet, 650, Aldeota' },
+  { name: 'Luis Eduardo', phone: '+55 88 994395211', address: 'Bloco do SMD, sala 1, Pici' },
+  { name: 'Paulo Victor', phone: '+55 85 99935449', address: 'Bloco do SMD, sala 2, Pici' },
 ];
 
 export default clients;
